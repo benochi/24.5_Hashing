0e67ba32ab14f4667b82ed92d8c90761267310a3
Dan P <73758508+benochi@users.noreply.github.com>
2021-08-19 11:31:21 -0500

    Add files via upload

---
+++ flask-feedback/models.py
@@ -0,0 +1,66 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+def connect_db(app):
+    """connect to database"""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """Class to make a user"""
+    __tablename__ = "users"
+
+    #id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), primary_key=True, nullable=False, unique=True)
+    password = db.Column(db.Text,  nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(0), nullable=False)
+    #relationship and backref for db. 
+    feedback = db.relationship("Feedback", backref="user", cascade="all,delete")
+
+    @classmethod
+    def register(cls, username, password, first_name, last_name, email):
+        """Register a user, hashing their password for DB storage."""
+
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode("utf8")
+        user = cls(
+            username=username,
+            password=hashed_utf8,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Validate that user exists & password is correct using bcrypt salt."""
+
+        user = User.query.filter_by(username=username).first() #username is unique don't use ".one()"
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
+        else:
+            return False
+
+class Feedback(db.Model):
+    """Feedback."""
+
+    __tablename__ = "feedback"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(
+        db.String(20),
+        db.ForeignKey('users.username'),
+        nullable=False,
+    )
